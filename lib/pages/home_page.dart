1738c87ebd130027cc84e40c9537c9ea6db4e017
Baraa AbuAlrob <b.abualrub3@student.aaup.edu>
2024-12-24 17:52:34 +0200

    Make the column scrollable.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,23 +8,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: EdgeInsets.all(MediaQuery.sizeOf(context).aspectRatio * 35.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
                 borderRadius: BorderRadius.circular(24.0),
                 child: Image(
                   image: const AssetImage('assets/images/classic_burger.jpg'),
                   height: MediaQuery.sizeOf(context).height * 0.23,
                   width: MediaQuery.sizeOf(context).width,
                   fit: BoxFit.cover,
-                )),
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.037,
-            ),
-            Expanded(
-              child: GridView.builder(
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.037,
+              ),
+              GridView.builder(
+                shrinkWrap: true,
                 itemCount: foods.length,
+                physics: const NeverScrollableScrollPhysics(),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   crossAxisSpacing: 8.0,
@@ -32,9 +35,9 @@ class HomePage extends StatelessWidget {
                 ),
                 itemBuilder: (context, index) =>
                     FoodGridItem(foodItem: foods[index]),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       );
 }
