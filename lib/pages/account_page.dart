5250421c5948977f769f6e83f082b19f3bf6ca87
Administrator <b.abualrub3@student.aaup.edu>
2026-07-20 23:12:33 +0300

    160 & 161. Changing the content on Landscape mode for the account page.

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -7,99 +7,170 @@ class AccountPage extends StatelessWidget {
     required BuildContext context,
     required String text,
     required int num,
-  }) =>
-      Column(
-        children: [
-          Text(
-            text,
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor,
-                ),
-          ),
-          Text(
-            num.toString(),
-            style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-        ],
-      );
+    required bool isLandscape,
+  }) => Column(
+    children: [
+      Text(
+        text,
+        style: isLandscape
+            ? Theme.of(context).textTheme.titleLarge!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              )
+            : Theme.of(context).textTheme.titleMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+      ),
+      Text(
+        num.toString(),
+        style: isLandscape
+            ? Theme.of(
+                context,
+              ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold)
+            : Theme.of(
+                context,
+              ).textTheme.titleSmall!.copyWith(fontWeight: FontWeight.bold),
+      ),
+    ],
+  );
 
   Widget itemsListTile({
     required BuildContext context,
     required String title,
     required IconData icon,
     String? subTitle,
-  }) =>
-      ListTile(
-        leading: Icon(
-          icon,
-          size: 30.0,
+  }) => ListTile(
+    leading: Icon(icon, size: 30.0),
+    title: Text(title),
+    subtitle: subTitle != null ? Text(subTitle) : null,
+    onTap: () => debugPrint('$title tapped!'),
+    trailing: const Icon(Icons.chevron_right, size: 30.0),
+  );
+
+  Widget accountPhoto({required double height, required double width}) =>
+      Container(
+        height: height,
+        width: width,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage('assets/images/mommy.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-        title: Text(
-          title,
-        ),
-        subtitle: subTitle != null ? Text(subTitle) : null,
-        onTap: () => debugPrint('$title tapped!'),
-        trailing: const Icon(
-          Icons.chevron_right,
-          size: 30.0,
-        ),
+      );
+
+  Widget nameText({required BuildContext context, required bool isLandscape}) =>
+      Text(
+        'Baraa Abu Al-Rob',
+        style: isLandscape
+            ? Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )
+            : Theme.of(context).textTheme.headlineMedium!.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
       );
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            Container(
-              width: 200.0,
-              height: 200.0,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/mommy.jpg'),
-                  fit: BoxFit.cover,
+    final isLandscape =
+        MediaQuery.orientationOf(context) == Orientation.landscape;
+    final size = MediaQuery.sizeOf(context);
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              if (!isLandscape) ...[
+                accountPhoto(height: size.width * 0.4, width: size.width * 0.5),
+                const SizedBox(height: 16.0),
+                nameText(context: context, isLandscape: isLandscape),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ordersAndVouchersDesign(
+                      context: context,
+                      text: 'Orders',
+                      num: 50,
+                      isLandscape: isLandscape,
+                    ),
+                    ordersAndVouchersDesign(
+                      context: context,
+                      text: 'Vouchers',
+                      num: 7,
+                      isLandscape: isLandscape,
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Baraa AbuAlrob',
-              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ordersAndVouchersDesign(
-                    context: context, text: 'Orders', num: 50),
-                ordersAndVouchersDesign(
-                    context: context, text: 'Vouchers', num: 7),
+                const SizedBox(height: 24.0),
               ],
-            ),
-            const SizedBox(height: 24.0),
-            const Divider(),
-            itemsListTile(
-              context: context,
-              title: 'Past Orders',
-              icon: Icons.shopping_cart,
-              subTitle: 'View your past orders',
-            ),
-            const Divider(),
-            itemsListTile(
-              context: context,
-              title: 'Available Vouchers',
-              icon: Icons.card_giftcard,
-              subTitle: 'View your available vouchers',
-            ),
-            const Divider(),
-          ],
+              if (isLandscape) ...[
+                Row(
+                  spacing: 32,
+                  mainAxisAlignment: .start,
+                  children: [
+                    SizedBox(width: 8),
+                    Column(
+                      spacing: 16,
+                      children: [
+                        accountPhoto(
+                          height: size.width * 0.2,
+                          width: size.width * 0.2,
+                        ),
+                        SizedBox(height: 8),
+                      ],
+                    ),
+                    Column(
+                      spacing: 24,
+                      mainAxisAlignment: .spaceEvenly,
+                      crossAxisAlignment: .start,
+                      children: [
+                        nameText(context: context, isLandscape: isLandscape),
+                        Row(
+                          spacing: size.width * 0.1,
+                          children: [
+                            ordersAndVouchersDesign(
+                              context: context,
+                              text: 'Orders',
+                              num: 50,
+                              isLandscape: isLandscape,
+                            ),
+                            ordersAndVouchersDesign(
+                              context: context,
+                              text: 'Vouchers',
+                              num: 7,
+                              isLandscape: isLandscape,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+              const Divider(),
+              itemsListTile(
+                context: context,
+                title: 'Past Orders',
+                icon: Icons.shopping_cart,
+                subTitle: 'View your past orders',
+              ),
+              const Divider(),
+              itemsListTile(
+                context: context,
+                title: 'Available Vouchers',
+                icon: Icons.card_giftcard,
+                subTitle: 'View your available vouchers',
+              ),
+              const Divider(),
+            ],
+          ),
         ),
       ),
     );
