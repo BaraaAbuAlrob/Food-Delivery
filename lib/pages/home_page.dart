9871ad770822d9ef099611a2c9eedf14766f714a
Administrator <b.abualrub3@student.aaup.edu>
2026-07-21 08:58:12 +0300

    163. Checking the platform device. && Write the import hierarchy.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,50 +1,59 @@
+// dart
+import 'dart:io';
+
+// Flutter
 import 'package:flutter/material.dart';
+
+// Other Packages
+
+// Internal Files
 import 'package:food_delivery/models/food_item.dart';
 import 'package:food_delivery/widgets/food_grid_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-
-    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     var size35 = MediaQuery.sizeOf(context).aspectRatio * 35.0;
     var size = MediaQuery.sizeOf(context);
     return Padding(
-        padding: EdgeInsets.only(left: size35, right: size35),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 16.0),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(24.0),
-                child: Image(
-                  image: const AssetImage('assets/images/classic_burger.jpg'),
-                  height: isLandscape ? size.height * 0.5 : size.height * 0.23,
-                  width: isLandscape ? size.width * 0.6 : size.width,
-                  fit: BoxFit.cover,
-                ),
+      padding: EdgeInsets.only(left: size35, right: size35),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 16.0),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(24.0),
+              child: Image(
+                image: const AssetImage('assets/images/classic_burger.jpg'),
+                height: isLandscape ? size.height * 0.5 : size.height * 0.23,
+                width: isLandscape ? size.width * 0.6 : size.width,
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: size.height * 0.037,
+            ),
+            SizedBox(height: size.height * 0.037),
+            if (Platform.isIOS) Text('IS an IOS device...'),
+            if (Platform.isAndroid) Text('IS an Android device...'),
+            SizedBox(height: size.height * 0.037),
+            GridView.builder(
+              shrinkWrap: true,
+              itemCount: foods.length,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: isLandscape ? 5 : 2,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 16.0,
+                childAspectRatio: 0.8,
               ),
-              GridView.builder(
-                shrinkWrap: true,
-                itemCount: foods.length,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: isLandscape ? 5 : 2,
-                  crossAxisSpacing: 10.0,
-                  mainAxisSpacing: 16.0,
-                  childAspectRatio: 0.8,
-                ),
-                itemBuilder: (context, index) =>
-                    FoodGridItem(foodIndex: index),
-              )
-            ],
-          ),
+              itemBuilder: (context, index) => FoodGridItem(foodIndex: index),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
